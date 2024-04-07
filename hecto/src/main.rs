c7dcb064e0bd6680e9c612bea4a0ccbf89a2007c
Pawel <108243071+melo1901@users.noreply.github.com>
2024-04-08 01:01:02 +0200

    features

--- hecto/src/main.rs
+++ hecto/src/main.rs
@@ -1,29 +1,14 @@
-use std::io::{self, stdout};
-use termion::event::Key;
-use termion::input::TermRead;
-use termion::raw::IntoRawMode;
-
-fn die(e: std::io::Error) {
-    panic!("{e}");
-}
+#![warn(clippy::all, clippy::pedantic)]
+mod document;
+mod editor;
+mod row;
+mod terminal;
+use document::Document;
+use editor::Editor;
+pub use editor::Position;
+pub use row::Row;
+pub use terminal::Terminal;
 
 fn main() {
-    let _stdout = stdout().into_raw_mode().unwrap();
-
-    for key in io::stdin().keys() {
-        match key {
-            Ok(key) => match key {
-                Key::Char(c) => {
-                    if c.is_control() {
-                        println!("{:?}\r", c as u8);
-                    } else {
-                        println!("{:?} ({})\r", c as u8, c);
-                    }
-                }
-                Key::Ctrl('q') => break,
-                _ => println!("{:?}\r", key),
-            },
-            Err(err) => die(err),
-        }
-    }
+    Editor::default().run()
 }
